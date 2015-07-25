1b634e5882a169a97e147e86603481353aa61e44
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-07-25 10:48:08 +0700

    Added: icon. Fixed: remove unnecessary string on age's text placeholder.

--- CatYears/ViewController.swift
+++ CatYears/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var convertedAgeTextField: UITextField!
+    @IBOutlet weak var convertedAgeLabel: UILabel!
+    @IBAction func convertAgeButton(sender: UIButton) {
+        convertedAgeTextField.resignFirstResponder()
+        if let ageToBeConverted = Int(convertedAgeTextField.text!) {
+            convertedAgeLabel.text = "Your cat is \(ageToBeConverted * 7) cat years old "
+        }
+        else {
+            convertedAgeLabel.text = "Please enter only valid number"
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
